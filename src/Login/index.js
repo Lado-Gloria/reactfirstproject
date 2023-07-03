9561688c77684d1c69238ddf406385737f76f051
Gloria-Lado <glorialadoo@gmail.com>
2023-07-03 17:01:59 +0300

    fixed

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,6 +1,5 @@
 import React,{useState} from "react";
 import { Link } from 'react-router-dom';
-import React from 'react';
 import './style.css';
 const Login =()=>{
         const [username,setUsername] =useState('');
