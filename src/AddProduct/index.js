62171249facac04f20948b104d912ffba369d400
Gloria-Lado <glorialadoo@gmail.com>
2023-07-05 12:08:39 +0300

    changes

--- src/AddProduct/index.js
+++ src/AddProduct/index.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
+import './style.css';
 import { useNavigate } from 'react-router-dom';
-
 
 const AddProduct = () => {
   const navigate = useNavigate();
   const [productName, setProductName] = useState('');
+  const [productImage, setProductImage] = useState('');
   const [productPrice, setProductPrice] = useState('');
+  const [productDiscount, setProductDiscount] = useState('');
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    
     const queryParams = new URLSearchParams({
       name: productName,
+      image: productImage,
       price: productPrice,
-   
+      discount: productDiscount,
     }).toString();
     
     navigate(`/products?${queryParams}`);
   };
+
   return (
-    <div>
+    <div className='add'>
       <h1>Add Product</h1>
-      <form onSubmit={handleSubmit}>
-        
+      <form onSubmit={handleSubmit} className='form'>
         <div>
           <label htmlFor="productName">Name:</label>
           <input
@@ -28,6 +33,16 @@ const AddProduct = () => {
             id="productName"
             value={productName}
             onChange={(event) => setProductName(event.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="productImage">Image:</label>
+          <input
+            type="text"
+            id="productImage"
+            value={productImage}
+            onChange={(event) => setProductImage(event.target.value)}
             required
           />
         </div>
@@ -41,9 +56,19 @@ const AddProduct = () => {
             required
           />
         </div>
+        <div>
+          <label htmlFor="productDiscount">Discount:</label>
+          <input
+            type="number"
+            id="productDiscount"
+            value={productDiscount}
+            onChange={(event) => setProductDiscount(event.target.value)}
+          />
+        </div>
         <button type="submit">Add</button>
       </form>
     </div>
   );
 };
+
 export default AddProduct;
